711a377d6db834bd555cc43daa32d44dd1b8f2af
Nazire Brown <nlb1@email.sc.edu>
2025-10-16 12:37:21 -0400

    react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>hey</h1>
+    <h2>love</h2>
+  </React.StrictMode>
+);
